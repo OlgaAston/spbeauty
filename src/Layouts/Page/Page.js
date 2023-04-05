a0ca802339db22b88a82f72076911e3a29f2d97d
OlgaAston <80826261+OlgaAston@users.noreply.github.com>
2023-04-05 21:28:24 +0000

    отступ сверху

--- src/Layouts/Page/Page.js
+++ src/Layouts/Page/Page.js
@@ -5,7 +5,7 @@ function Page() {
   return (
     <div className='flex-grow-1'>
       <Header/>
-      <div className='container pl--72 pr--72'>  {/* Bootstrap контейнер */}
+      <div className='container pl--72 pr--72 mt-5'>  {/* Bootstrap контейнер */}
         <AboutCompany/>  {/* Страница выбирается нажатием на сайдбаре. Сейчас это только AboutCOmpany. */}
       </div>
     </div>
