b615d1a8e17c8fcc73923b822f3850de8e97f521
OlgaAston <80826261+OlgaAston@users.noreply.github.com>
2023-04-05 19:22:24 +0000

    Исправила отступы полей ввода/выбора

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -23,7 +23,7 @@ function TitledSelector({title, options}) {
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Select className='p-2'>
+            <Select className='px-3 py-2'>
                 {options.map(optionName => {
                     return (
                         <option key={optionName}>{optionName}</option>
