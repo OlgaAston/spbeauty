95f057ecc0b45393a15bf66716f74423e1f3ee43
OlgaAston <80826261+OlgaAston@users.noreply.github.com>
2023-04-05 19:17:12 +0000

    Исправила отступы поля поиска

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -26,7 +26,7 @@ const SearchIconWrapper = styled.div`
     background-color: none;
     position: absolute;
     box-sizing:border-box;
-    right: 8px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
+    right: 2.5px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
     top:50%;
     transform: translateY(-50%);
 `;
@@ -34,8 +34,8 @@ const SearchIconWrapper = styled.div`
 function SearchField() {
     return (
         <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
-            <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
+            <Input className='px-3 py-2' placeholder='Найти на странице...'/>
+            <SearchIconWrapper className='px-3 py-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
                 <IconOnlyButton icon={<SearchIcon/>}/>
             </SearchIconWrapper>
         </WrapperInput>
